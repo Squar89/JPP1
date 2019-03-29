2e5501ff0eca84816a14c47e7b1b79c3a5eaf836
Jakub Wróblewski <j.wroblewski97@gmail.com>
2019-03-29 21:15:51 +0100

    Compilable lib before testing

--- Lib.hs
+++ Lib.hs
@@ -7,7 +7,7 @@ main = interact cat
 
 type R = Rational
 type R2 = (R,R)
-type R2Line = (R2, R2)
+type Line = (R2, R2)
 type IntLine = ((Int,Int), (Int,Int))
 type IntRendering = [IntLine]
 
@@ -16,7 +16,7 @@ data Vec = V R2
 -- punkt 2D
 data Point = P R2
 -- rysunek złożony z linii
-data Picture = Pic [R2Line]
+data Picture = Pic [Line]
 -- transformacja czyli translacja o wektor (Vec) lub rotacja o liczbę (R)
 data Transform = T [(Either Vec R)]
 
@@ -45,12 +45,14 @@ instance Mon Transform where
       | otherwise = list1 ++ list2
       where
         last1 = last list1 
-        mergedRot = [Right (minimizeDeg (fromRight last1 + fromRight h2))]
+        mergedRot = [Right (normalizeDeg (fromRight last1 + fromRight h2))]
 
+-- funkcja zapożyczona z jednej z bibliotek
 isRight :: Either a b -> Bool
 isRight (Left  _) = False
 isRight (Right _) = True
 
+-- zmodyfikowana funkcja zapożyczona z jednej z bibliotek
 fromRight :: Either Vec R -> R
 fromRight (Right b) = b
 fromRight _         = 0
@@ -61,11 +63,11 @@ point r2 = P r2
 vec :: R2 -> Vec
 vec r2 = V r2
 
-minimizeDeg :: R -> R
-minimizeDeg deg
-  | deg >= -fullCircle && deg <= fullCircle = deg
-  | deg > fullCircle                        = minimizeDeg (deg - fullCircle)
-  | deg < -fullCircle                       = minimizeDeg (deg + fullCircle)
+normalizeDeg :: R -> R
+normalizeDeg deg
+  | deg >= 0 && deg <= fullCircle = deg
+  | deg > fullCircle              = normalizeDeg (deg - fullCircle)
+  | deg < 0                       = normalizeDeg (deg + fullCircle)
 
 -- odcinek pomiędzy punktami o podanych współrzędnych
 line :: (R,R) -> (R,R) -> Picture
@@ -97,22 +99,61 @@ translate v = T [Left v]
 -- obrót wokół punktu (0,0) przeciwnie do ruchu wskazówek zegara
 -- jednostki mozna sobie wybrać
 rotate :: R -> Transform
-rotate deg = T [Right (minimizeDeg deg)]
+rotate deg = T [Right (normalizeDeg deg)]
 
 -- wartość odpowiadająca 1 pełnemu obrotowi (360 stopni)
 fullCircle :: R
 fullCircle = 360
 
+-- wyliczam wartość sinusa ze wzoru Bhaskary
+getSine :: R -> R
+getSine deg
+  | deg < 0                = getSine (deg + 360)
+  | deg >= 0 && deg <= 180 = (4 * deg * (180 - deg)) / (40500 - (deg * (180 - deg)))
+  | otherwise              = -(getSine (deg - 180))
+
+-- wyliczam wartość cosinusa z zależności z sinusem: cos(x) = sin(90-x)
+getCosine deg = getSine(90 - deg)
+
+rotatePoint :: Point -> R -> Point
+rotatePoint (P (x, y)) deg = P (x', y') where
+  x' = x * (getCosine deg) - y * (getSine deg)
+  y' = y * (getCosine deg) + x * (getSine deg)
+
+rotateVector :: Vec -> R -> Vec
+rotateVector (V (x, y)) deg = V (x', y') where
+  x' = x * (getCosine deg) - y * (getSine deg)
+  y' = y * (getCosine deg) + x * (getSine deg)
+
+translatePoint :: Point -> Vec -> Point
+translatePoint (P (x, y)) (V (vx, vy)) = P (x + vx, y + vy)
+
 -- transformacja wykonana na podanym punkcie
----trpoint :: Transform -> Point -> Point
+trpoint :: Transform -> Point -> Point
+trpoint (T []) p = p
+trpoint (T (h:t)) p = 
+  case h of
+    Left vec -> trpoint (T t) (translatePoint p vec)
+    Right deg -> trpoint (T t) (rotatePoint p deg)
 
 -- transformacja wykonana na podanym wektorze
 -- w przypadku gdy transformacja jest wektorem, to jest to funkcja id
 -- jesli transformacja jest rotacją to modyfikuje wektor
----trvec :: Transform -> Vec -> Vec
+trvec :: Transform -> Vec -> Vec
+trvec (T []) vec = vec
+trvec (T (h:t)) vec = 
+  case h of
+    Left vec -> trvec (T t) vec
+    Right deg -> trvec (T t) (rotateVector vec deg)
 
 -- transformacja wykonana na podanym rysunku
----transform :: Transform -> Picture -> Picture
+transform :: Transform -> Picture -> Picture
+transform (T []) pic = pic
+transform t (Pic []) = (Pic [])
+transform t (Pic picList) = (Pic (map transformList picList)) where
+  transformList (start, end) = (transStart, transEnd) where
+    (P transStart) = trpoint t (P start)
+    (P transEnd) = trpoint t (P end)
 
 
 
